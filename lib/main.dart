31fb919d404c23353c184508870035651e78345a
Liben <adugnaliben65@gmail.com>
2025-05-18 17:31:45 +0300

    Initialize services and controllers in main.dart; fix import statement in player_hand.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,15 +3,26 @@ import 'package:get/get.dart';
 import 'package:crazygame/routes/app_pages.dart';
 import 'package:crazygame/theme/app_theme.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:crazygame/controllers/game_controller.dart';
+import 'package:crazygame/controllers/settings_controller.dart';
+import 'package:crazygame/services/websocket_service.dart';
+import 'package:crazygame/services/voice_chat_service.dart';
 import 'firebase_options.dart';
-
-
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
-);
+  );
+
+  // Initialize services
+  Get.put(WebSocketService());
+  Get.put(VoiceChatService());
+
+  // Initialize controllers
+  Get.put(SettingsController());
+  Get.put(GameController());
+
   runApp(const CrazyGameApp());
 }
 
--- lib/screens/game/components/player_hand.dart
+++ lib/screens/game/components/player_hand.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart'
 import 'package:get/get.dart';
 import 'package:flutter_card_swiper/flutter_card_swiper.dart';
 import 'package:crazygame/models/game_state.dart' as game;
