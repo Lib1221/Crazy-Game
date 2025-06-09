35493164b5c8dc9d28b111577d64724463a79447
Liben <adugnaliben65@gmail.com>
2025-06-09 22:56:01 +0300

    Enhance CardGameRuleChecker with additional card validation methods
    - Added methods to check if a card is a wild card (8 or Jack), an Ace, or a 2.
    - Implemented getNumberFromCard method to retrieve the card number from a PlayingCard object.
    - Improved existing card number validation logic for Jokers.
    - Enhanced code organization and clarity within the CardGameRuleChecker class.

--- lib/services/game/card_game_rule.dart
+++ lib/services/game/card_game_rule.dart
@@ -7,10 +7,12 @@ class CardGameRuleChecker {
       throw ArgumentError('Card number must be between 1 and 54');
     }
 
-    if (number == 53)
+    if (number == 53) {
       return PlayingCard(Suit.joker, CardValue.ace); // Red Joker
-    if (number == 54)
+    }
+    if (number == 54) {
       return PlayingCard(Suit.joker, CardValue.king); // Black Joker
+    }
 
     final suits = [Suit.spades, Suit.hearts, Suit.diamonds, Suit.clubs];
     final values = [
@@ -50,7 +52,58 @@ class CardGameRuleChecker {
       return false;
     }
 
+    // If the card being played on (param2) is 8 or Jack, any card can be played
+    if (card2.value == CardValue.eight || card2.value == CardValue.jack) {
+      return true;
+    }
+
     // Check for same suit or same value
     return card1.suit == card2.suit || card1.value == card2.value;
+  }
+
+  /// Checks if a card is a wild card (8 or Jack)
+  static bool isWildCard(int number) {
+    final card = getCardFromNumber(number);
+    return card.value == CardValue.eight || card.value == CardValue.jack;
+  }
+
+  /// Gets the number from a card
+  static int getNumberFromCard(PlayingCard card) {
+    if (card.suit == Suit.joker) {
+      return card.value == CardValue.ace ? 53 : 54;
+    }
+
+    final suits = [Suit.spades, Suit.hearts, Suit.diamonds, Suit.clubs];
+    final values = [
+      CardValue.ace,
+      CardValue.two,
+      CardValue.three,
+      CardValue.four,
+      CardValue.five,
+      CardValue.six,
+      CardValue.seven,
+      CardValue.eight,
+      CardValue.nine,
+      CardValue.ten,
+      CardValue.jack,
+      CardValue.queen,
+      CardValue.king
+    ];
+
+    int suitIndex = suits.indexOf(card.suit);
+    int valueIndex = values.indexOf(card.value);
+    return suitIndex * 13 + valueIndex + 1;
+  }
+
+  /// Checks if a card is an Ace
+  static bool isAce(int number) {
+    final card = getCardFromNumber(number);
+    return card.value == CardValue.ace;
+  }
+
+  /// Checks if a card is a 2
+  static bool isTwo(int number) {
+    final card = getCardFromNumber(number);
+    return card.value == CardValue.two;
   }
 }
